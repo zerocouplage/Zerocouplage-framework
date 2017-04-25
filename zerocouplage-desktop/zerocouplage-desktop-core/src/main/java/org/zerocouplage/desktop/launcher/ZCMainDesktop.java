a02c725e88dafe05994d421675a63c13f44e4bef
ettifouri el hassane <ehettifouri@sqli.com>
2017-04-25 20:54:54 +0100

    Correction bug Initialisation JavaFX.

--- zerocouplage-desktop/zerocouplage-desktop-core/src/main/java/org/zerocouplage/desktop/launcher/ZCMainDesktop.java
+++ zerocouplage-desktop/zerocouplage-desktop-core/src/main/java/org/zerocouplage/desktop/launcher/ZCMainDesktop.java
@@ -6,12 +6,16 @@ import org.zerocouplage.api.controller.IZCManager;
 import org.zerocouplage.impl.config.ZeroCouplageConfigImpl;
 import org.zerocouplage.impl.controller.ZCManagerFactory;
 
+import com.sun.javafx.application.PlatformImpl;
+
 public class ZCMainDesktop {
 
 	public static void main(String[] args) throws ClassNotFoundException,
 			InstantiationException, IllegalAccessException {
 
 		ZCMainDesktop ViewVide = new ZCMainDesktop();
+		//Initialization du JavaFx.
+		PlatformImpl.startup(() -> {});
 		IZCManager manager = ZCManagerFactory.getNewManager(ViewVide );
 		IZeroCouplageConfig zcConfig = ZeroCouplageConfigImpl.getInstance();
 		zcConfig.getLoaderConfig().setContext("desktop");
